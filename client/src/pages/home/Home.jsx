198be121de870121bbca0e52c19651e4e333130a
IrtazaAhmed147 <irtazaahmedk@gmail.com>
2025-07-10 16:18:35 +0500

    home page , footer, project card

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,18 +3,114 @@ import './Home.scss'
 import Featured from '../../components/featured/Featured'
 import TrustedBy from '../../components/trustedBy/TrustedBy'
 import Slide from '../../components/slide/Slide'
-import { cards } from '../../data'
+import { cards, projects } from '../../data'
 import CatCard from '../../components/catCard/CatCard'
+import ProjectCard from '../../components/projectCard/ProjectCard'
 function Home() {
   return (
     <div className='home'>
       <Featured />
       <TrustedBy />
-      <Slide  slidesToShow={5} arrowsScroll={5}>
-        {cards.map((card)=> (
-            <CatCard key={card.id} card={card}/>
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
         ))}
       </Slide>
+
+
+      <div className="features">
+        <div className="container">
+          <div className="item">
+
+            <h1>A whole world of freelance talent at your fingertips</h1>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              The best for every budget
+            </div>
+
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Quality work done quickly
+            </div>
+            <p>
+              Find the right freelancer to begin working on your project within
+              minutes.
+            </p>
+
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Protected payments, every time
+            </div>
+            <p>
+              Always know what you'll pay upfront. Your payment isn't released
+              until you approve the work.
+            </p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              24/7 support
+            </div>
+            <p>
+              Find high-quality services at every price point. No hourly rates,
+              just project-based pricing.
+            </p>
+
+
+          </div>
+
+
+          <div className='item'>
+            <video src="" controls></video>
+
+          </div>
+
+
+        </div>
+      </div>
+
+      {/* part 2 */}
+
+      <div className="features dark">
+        <div className="container">
+          <div className="item">
+
+
+            <h1>fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>Upgrade to a curated experience packed with tools and benefits, dedicated to businesses</p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Connect to freelancers with proven business experience
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Get matched with the perfect talent by a customer success manager
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Manage teamwork with boost productivity with one powerful workspace
+            </div>
+            <button>Explore Fiverr Business</button>
+          </div>
+
+          <div className='item'>
+            <img src="" alt="" />
+          </div>
+
+        </div>
+      </div>
+
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {projects.map((card) => (
+          <ProjectCard key={card.id} card={card} />
+        ))}
+      </Slide>
+
     </div>
   )
 }
