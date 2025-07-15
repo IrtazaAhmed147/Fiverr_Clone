75ff4a2d2934dce9ebb7d9314f6909bd0ce0e236
IrtazaAhmed147 <irtazaahmedk@gmail.com>
2025-07-16 00:12:39 +0500

    gig route and controller

--- api/routes/gig.route.js
+++ api/routes/gig.route.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { deleteUser } from "../controllers/user.controller.js";
+import {
+  createGig,
+  deleteGig,
+  getGig,
+  getGigs
+} from "../controllers/gig.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-// router.delete("/:id", (req, res)=> {
 
-// });
-router.get("/:id", deleteUser);
+router.post("/", verifyToken, createGig);
+router.delete("/:id", verifyToken, deleteGig);
+router.get("/single/:id", getGig);
+router.get("/", getGigs);
+
 
 export default router;
